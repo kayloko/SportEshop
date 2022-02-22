8f0d19261301f8b6140ebeab3afdbf380ca97baa
kayloko <cherif.aymen@esprit.tn>
2022-02-22 15:38:13 +0100

    hello got

---
+++ src/Entity/Commande.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommandeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CommandeRepository::class)
+ */
+class Commande
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $id_produit;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdProduit(): ?int
+    {
+        return $this->id_produit;
+    }
+
+    public function setIdProduit(int $id_produit): self
+    {
+        $this->id_produit = $id_produit;
+
+        return $this;
+    }
+}
